cf2f7adf001739923f1f91895f48deaf6a6210c6
shamotte <123325697+shamotte@users.noreply.github.com>
2025-05-12 14:35:39 +0200

    AAAAAAAAA

--- Code/functions.cpp
+++ Code/functions.cpp
@@ -13,12 +13,3 @@
 #include "states.h"
 
 extern int RANK, SIZE;
-template <typename... Args>
-void coutcolor(Args &&...args)
-{
-    std::ostringstream oss;
-    (oss << ... << args); // składnia fold expression (C++17)
-    std::cout << "\033[0;" << (31 + RANK % 7) << "m"
-              << oss.str()
-              << "\033[0m\n";
-}
--- Code/main.cpp
+++ Code/main.cpp
@@ -17,6 +17,15 @@
 MPI_Datatype my_data;
 
 int RANK, SIZE;
+template <typename... Args>
+void coutcolor(Args &&...args)
+{
+    std::ostringstream oss;
+    (oss << ... << args); // składnia fold expression (C++17)
+    std::cout << "\033[0;" << (31 + RANK % 7) << "m"
+              << oss.str()
+              << "\033[0m\n";
+}
 
 void check_thread_support(int provided)
 {
