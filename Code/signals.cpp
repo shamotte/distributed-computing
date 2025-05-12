b931247188d9e830249f4e546037f40471273c38
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-05-12 20:03:15 +0200

    cleanup-er

---
+++ Code/signals.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <mutex>
+#include <string>
+#include <set>
+#include <sstream>
+
+#include "config.h"
+#include "functions.h"
+#include "states.h"
+
+#include <mpi.h>
+
+extern int RANK, SIZE;
+extern unsigned int global_lamport;
+extern MPI_Datatype my_data;
+
+void Broadcast_SIG_TABLE_REQ(int priority, int vote)
+{
+    global_lamport++;
+
+    Datatype d;
+    d.type = SIG_TABLE_REQ;
+    d.lamport = global_lamport;
+    d.pid = RANK;
+
+    d.priority = priority;
+    d.vote = vote;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+
+        MPI_Send(&d, 1, my_data, i, 0, MPI_COMM_WORLD);
+    }
+}
+
+void Send_SIG_SIG_TABLE_ACK(int dest)
+{
+    global_lamport++;
+}
+
+void Send_SIG_TABLE(int dest, std::set<int> companions, int table_number, int chosen_game)
+{
+    global_lamport++;
+
+    Datatype d;
+    d.type = SIG_TABLE;
+    d.lamport = global_lamport;
+    d.pid = RANK;
+
+    std::copy(companions.begin(), companions.end(), d.players);
+
+    d.table_number = table_number;
+    d.chosen_game = chosen_game;
+
+    MPI_Send(&d, 1, my_data, dest, 0, MPI_COMM_WORLD);
+}
+
+void Send_SIG_END_REQ(int dest)
+{
+    global_lamport++;
+
+	Datatype d;
+	d.type = SIG_END_REQ;
+	d.lamport = global_lamport;
+    d.pid = RANK;
+
+	MPI_Send(&d, 1, my_data, dest, 0, MPI_COMM_WORLD);
+
+} // zgłoszenie gotowości do zakończenia gry
+
+void Broadcast_SIG_GAME_END(std::set<int> players, int table_number)
+{
+    global_lamport++;
+}
