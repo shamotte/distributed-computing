47c5ac4d428e14bb798b5c3a2f5ba134e87edb23
shamotte <123325697+shamotte@users.noreply.github.com>
2025-05-13 18:32:39 +0200

    Update states.cpp

--- Code/states.cpp
+++ Code/states.cpp
@@ -58,7 +58,7 @@ void BaseState::ProcessSIG_TABLE_REQ(Datatype &d)
                      { return d.priority * SIZE + d.pid < p.priority * SIZE + p.pid; }),
         QueuePosition{d.pid, d.priority, d.vote});
 
-    ctx->players_acknowledged[d.pid] = true;
+    ctx->players_acknowledged[d.pid] = d.lamport;
 
     ctx->cv_seek.notify_all();
 
@@ -67,7 +67,7 @@ void BaseState::ProcessSIG_TABLE_REQ(Datatype &d)
 
 void BaseState::ProcessSIG_SIG_TABLE_ACK(Datatype &d)
 {
-    ctx->players_acknowledged[d.pid] = true;
+    ctx->players_acknowledged[d.pid] = d.lamport;
     ctx->cv_seek.notify_all();
 }
 
@@ -159,8 +159,9 @@ void StateSeek::Logic()
     std::unique_lock lock(x);
 
     ctx->cv_seek.wait(lock, [this]()
-                    { 
+                      { 
                         std::stringstream ss;
+                        ss<"M: "<< ctx->priority<<" , ";
                         for(bool b : ctx->players_acknowledged)
                         {
                             ss<<b<<" ";
@@ -169,18 +170,20 @@ void StateSeek::Logic()
 
                         return std::all_of(this->ctx->players_acknowledged,
                                         this->ctx->players_acknowledged + PLAYER_NUM,
-                                        [](bool b)
-                                        { return b; }); });
+                                        [this](int b)
+                                        { return ctx->priority < b; }); });
 
     coutcolor(RANK, " wlaskie zakonczyl czekanie");
 
-    while (true) {
-
+    while (true)
+    {
 
         auto &queue = ctx->queue;
-        for (int pos = 0; pos < queue.size(); pos += SEAT_COUNT) {
+        for (int pos = 0; pos < queue.size(); pos += SEAT_COUNT)
+        {
             int table_index = pos / SEAT_COUNT;
-            if (pos + SEAT_COUNT > queue.size()) {
+            if (pos + SEAT_COUNT > queue.size())
+            {
                 coutcolor(RANK, " nie ma wystarczającej liczby graczy");
                 break;
             }
@@ -192,23 +195,27 @@ void StateSeek::Logic()
 
             bool is_last = RANK == queue[last_player_index].pid;
             coutcolor(RANK, "jestem ostatni? (", queue[last_player_index].pid, ")", is_last);
-            if (is_last) {
+            if (is_last)
+            {
 
                 // Obierz stół
-                for (auto tid: ctx->table_numbers) {
+                for (auto tid : ctx->table_numbers)
+                {
                     coutcolor("stół", tid);
                 }
                 ctx->table_number = ctx->table_numbers[table_index];
 
                 // Wykryj współgraczy
                 ctx->companions.clear();
-                for (int i = first_player_index; i <= last_player_index; i++) {
+                for (int i = first_player_index; i <= last_player_index; i++)
+                {
                     ctx->companions.insert(queue[i].pid);
                 }
 
                 // Zlicz głosy
                 int votes[SEAT_COUNT] = {};
-                for (int i = table_index * SEAT_COUNT; i < table_index * SEAT_COUNT + SEAT_COUNT; i++) {
+                for (int i = table_index * SEAT_COUNT; i < table_index * SEAT_COUNT + SEAT_COUNT; i++)
+                {
                     votes[queue[i].vote]++;
                 }
 
@@ -217,8 +224,10 @@ void StateSeek::Logic()
                 ctx->chosen_game = chosen_game;
 
                 // Wysłanie SIG_TABLE
-                for (auto comp : ctx->companions) {
-                    if (comp != RANK) {
+                for (auto comp : ctx->companions)
+                {
+                    if (comp != RANK)
+                    {
                         Send_SIG_TABLE(comp, ctx->companions, ctx->table_number, chosen_game);
                     }
                 }
@@ -233,14 +242,16 @@ void StateSeek::Logic()
         coutcolor(RANK, " nie znaleziiono stołu");
 
         // Zakończ stan jeśli ustawiono na PLAY - failsafe
-        if (ctx->next_state == STATE_PLAY) {
+        if (ctx->next_state == STATE_PLAY)
+        {
             return;
         }
 
         ctx->cv_seek_wake.wait(lock);
 
         // Zakończ stan jeśli ustawiono na PLAY
-        if (ctx->next_state == STATE_PLAY) {
+        if (ctx->next_state == STATE_PLAY)
+        {
             return;
         }
     }
@@ -259,12 +270,10 @@ void StatePlay::Logic()
 {
     coutcolor("zmienilem stan na PLAY");
 
-    // HUH?
-    memset(ctx->players_acknowledged, 0, PLAYER_NUM);
+        std::this_thread::sleep_for(std::chrono::seconds(rand() % 10));
 
-    std::this_thread::sleep_for(std::chrono::seconds(rand() % 10));
-
-    for (auto comp: ctx->companions) {
+    for (auto comp : ctx->companions)
+    {
         Send_SIG_END_REQ(comp);
     }
 
@@ -272,12 +281,12 @@ void StatePlay::Logic()
     std::unique_lock lock(x);
 
     ctx->cv_game_end_req.wait(lock, [this]()
-        { return this->ctx->end_ready == SEAT_COUNT; }
-    );
+                              { return this->ctx->end_ready == SEAT_COUNT; });
 
     coutcolor("Gra zakończona!");
 
-    if (RANK == *std::min_element(ctx->companions.begin(), ctx->companions.end())) {
+    if (RANK == *std::min_element(ctx->companions.begin(), ctx->companions.end()))
+    {
         coutcolor("GAME OVER, GO HOME.");
         Broadcast_SIG_GAME_END(ctx->companions, ctx->table_number);
     }
