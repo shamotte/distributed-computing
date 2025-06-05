57cd21ffcfaa14bf4a734cca7d791dd82c1e4c88
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-06-05 11:34:20 +0200

    Tweaks

--- Code/config.h
+++ Code/config.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define PLAYER_NUM 12
-#define TABLE_NUM 3
+#define PLAYER_NUM 8
+#define TABLE_NUM 4
 #define GAME_NUM 3
-#define SEAT_COUNT 4
+#define SEAT_COUNT 2
 #define MAX_SLEEP_MS 500
 
-#define DEBUG 0
+#define DEBUG 1
